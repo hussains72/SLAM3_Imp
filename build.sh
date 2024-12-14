a247331e255fcc82e9bf137bedb108e938e0d18e
sajid <smilesajid14@gmail.com>
2024-12-14 14:20:54 +0800

    Modified ORB-SLAM3 code

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd Thirdparty/DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j4
 
 cd ../../g2o
 
@@ -13,8 +13,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
-
+make -j4
 cd ../../Sophus
 
 echo "Configuring and building Thirdparty/Sophus ..."
@@ -22,7 +21,7 @@ echo "Configuring and building Thirdparty/Sophus ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j4
 
 cd ../../../
 
